3760d70f674d65e500cc60081309bc4f77ddfb61
LayMorja <itkv@sk.ru>
2021-03-01 17:26:12 +0300

    Выполнил задания первой практики

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let money = 25;
+let income = "";
+let addExpenses = 'Парарам';
+let deposit = [1, 2, 3];
+let mission = 'Невыполнима';
+let period = 38;
+
+alert('Привет, как у тебя дела?');
+
+console.log('Всё, в принципе, нормально');
