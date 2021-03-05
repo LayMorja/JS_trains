4c17979ef4a4f55b3fc8d6159fc41b2601cc25e4
LayMorja <itkv@sk.ru>
2021-03-05 17:44:24 +0300

    Завершил второе задание

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
 let money = 60000;
 let income = 'фриланс';
-let addExpenses = 'интернет, такси, коммуналка';
+let addExpenses = 'Интернет, такси, коммуналка';
 let deposit = true;
 let mission = 3000000;
 let period = 12;
+let budgetDay = money / 30;
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 console.log(addExpenses.length);
+console.log('Период равен - ', period, 'месяцев');
+console.log('Цель заработать ', mission, 'долларов');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
