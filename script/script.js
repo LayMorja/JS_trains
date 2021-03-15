b760f30d21710484e470de8d5dc77be43d1928e9
LayMorja <itkv@sk.ru>
2021-03-15 18:09:45 +0300

    Начал третий урок

--- script/script.js
+++ script/script.js
@@ -1,16 +1,7 @@
-let money = 60000;
-let income = 'фриланс';
-let addExpenses = 'Интернет, такси, коммуналка';
-let deposit = true;
-let mission = 3000000;
-let period = 12;
-let budgetDay = money / 30;
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен - ', period, 'месяцев');
-console.log('Цель заработать ', mission, 'долларов');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay);
+let money = +prompt('Каков Ваш месячный доход?');
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = prompt('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов');
+let expenses2 = prompt('Введите обязательную статью расходов');
+let amount1 = prompt('Во сколько это обойдется?');
+let amount2 = prompt('Во сколько это обойдется?');
