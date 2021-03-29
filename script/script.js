3df11af56a0222c3bba311415846a1d22ab38b9c
LayMorja <itkv@sk.ru>
2021-03-29 18:12:59 +0300

    Выполнил задания третьего урока

--- script/script.js
+++ script/script.js
@@ -1,7 +1,34 @@
+'use strict';
 let money = +prompt('Каков Ваш месячный доход?');
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = prompt('Есть ли у вас депозит в банке?');
+let deposit = prompt('Есть ли у вас депозит в банке?', 'нет');
 let expenses1 = prompt('Введите обязательную статью расходов');
+let amount1 = +prompt('Во сколько это обойдется?');
 let expenses2 = prompt('Введите обязательную статью расходов');
-let amount1 = prompt('Во сколько это обойдется?');
-let amount2 = prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let mission = +prompt('Какова цель Ваших сбережений?')
+console.log('Ваш месячный доход: ', money, 'рублей');
+console.log('Ваши расходы за расчитываемый период: ', addExpenses);
+switch (deposit) {
+  case 'да':
+  case 'есть':
+    console.log(true);
+    break;
+  default:
+    console.log(false);
+}
+console.log('Обязательная статья расходов: ', expenses1);
+console.log('Она обойдется в: ', amount1);
+console.log('Обязательная статья расходов: ', expenses2);
+console.log('Она обойдется в: ', amount2);
+let budgetMonth = money - amount1 - amount2;
+console.log('Ваш бюджет на месяц: ', budgetMonth, 'рублей');
+console.log('Цель ваших накоплений: ', mission, 'рублей');
+console.log('Вы достигните своей цели за', Math.ceil(mission / budgetMonth), 'месяцев');
+let budgetDay = budgetMonth / 30;
+console.log('Ваш дневной бюджет: ', Math.floor(budgetDay));
+
+if (budgetDay >= 1200) console.log('У Вас высокий уровень дохода');
+if (1200 > budgetDay > 600) console.log('У Вас средний уровень дохода');
+if (600 >= budgetDay > 0) console.log('У Вас низкий уровень дохода');
+if (budgetDay < 0) console.log('Что-то пошло не так :/');
