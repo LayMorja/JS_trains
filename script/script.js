4e22662290d3a7a9eb634f43172ba62f1c615326
LayMorja <LayMorja@users.noreply.github.com>
2021-04-05 17:48:44 +0300

    Выполнил первое дополнительное задание

---
+++ script/script.js
@@ -0,0 +1,13 @@
+let lang = prompt('Введите "ru" или "en"');
+switch (lang) {
+  case 'ru':
+    console.log('Понедельник \n Вторник \n Среда \n Четверг \n Пятница \n Суббота \n Воскресенье');
+    document.getElementById('tag-list').innerHTML = '<li>Понедельник<li>Вторник<li>Среда<li>Четверг<li>Пятница<li>Суббота<li>Воскресенье';
+    break;
+  case 'en':
+    console.log('Monday \n Tuesday \n Wednesday \n Thursday \n Friday \n Saturday \n Sunday');
+    document.getElementById('tag-list').innerHTML = '<li>Monday<li>Tuesday<li>Wednesday<li>Thursday<li>Friday<li>Saturday<li>Sunday';
+    break;
+  default:
+    console.log('А ведь указали, что писать');
+}
