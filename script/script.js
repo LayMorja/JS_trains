f1558b28d1521f1375a3c326704bab5f59212b42
LayMorja <LayMorja@users.noreply.github.com>
2021-04-05 17:59:16 +0300

    Выполнил дополнительное задание

--- script/script.js
+++ script/script.js
@@ -1,4 +1,10 @@
+'use strict';
 let lang = prompt('Введите "ru" или "en"');
+
+let namePerson = prompt('Введите имя', 'Артём');
+namePerson === 'Артём' ? console.log('Директор') : console.log('Студент');
+namePerson === 'Максим' ? console.log('Преподователь') : console.log('Студент');
+
 switch (lang) {
   case 'ru':
     console.log('Понедельник \n Вторник \n Среда \n Четверг \n Пятница \n Суббота \n Воскресенье');
