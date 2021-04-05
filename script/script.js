a953d271f9df40af978cabe6c6f8d7656d382ee3
LayMorja <evil-corporaty@yandex.ru>
2021-04-05 18:43:14 +0300

    Сделал код покрасивее

--- script/script.js
+++ script/script.js
@@ -1,14 +1,21 @@
 'use strict';
-let money = +prompt('Каков Ваш месячный доход?');
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = prompt('Есть ли у вас депозит в банке?', 'нет');
-let expenses1 = prompt('Введите обязательную статью расходов');
-let amount1 = +prompt('Во сколько это обойдется?');
-let expenses2 = prompt('Введите обязательную статью расходов');
-let amount2 = +prompt('Во сколько это обойдется?');
-let mission = +prompt('Какова цель Ваших сбережений?')
+let money = +prompt('Каков Ваш месячный доход?'),
+
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+
+    deposit = prompt('Есть ли у вас депозит в банке?', 'нет'),
+
+    expenses1 = prompt('Введите обязательную статью расходов'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+
+    expenses2 = prompt('Введите обязательную статью расходов'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+
+    mission = +prompt('Какова цель Ваших сбережений?');
+
 console.log('Ваш месячный доход: ', money, 'рублей');
 console.log('Ваши расходы за расчитываемый период: ', addExpenses);
+
 switch (deposit) {
   case 'да':
   case 'есть':
@@ -17,15 +24,20 @@ switch (deposit) {
   default:
     console.log(false);
 }
+
 console.log('Обязательная статья расходов: ', expenses1);
 console.log('Она обойдется в: ', amount1);
+
 console.log('Обязательная статья расходов: ', expenses2);
 console.log('Она обойдется в: ', amount2);
+
 let budgetMonth = money - amount1 - amount2;
 console.log('Ваш бюджет на месяц: ', budgetMonth, 'рублей');
+
 console.log('Цель ваших накоплений: ', mission, 'рублей');
 console.log('Вы достигните своей цели за', Math.ceil(mission / budgetMonth), 'месяцев');
 let budgetDay = budgetMonth / 30;
+
 console.log('Ваш дневной бюджет: ', Math.floor(budgetDay));
 
 if (budgetDay >= 1200) {
