1aaced424846873e1cd115049d397d1de17e03aa
LayMorja <evil-corporaty@yandex.ru>
2021-04-12 21:45:10 +0300

    Очистил скрипт

--- script/script.js
+++ script/script.js
@@ -1,22 +1,18 @@
 'use strict';
-let lang = prompt('Введите "ru" или "en"');
+// let lang = prompt('Введите "ru" или "en"');
 
-let namePerson = prompt('Введите имя', 'Артём');
-namePerson === 'Артём' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподователь') : console.log('Студент');
-<<<<<<< HEAD
-;
-=======
->>>>>>> f0112ab (Исправил ошибку тернарных операторов (создал вложенность))
+// let namePerson = prompt('Введите имя', 'Артём');
+// namePerson === 'Артём' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподователь') : console.log('Студент');
 
-switch (lang) {
-  case 'ru':
-    console.log('Понедельник \n Вторник \n Среда \n Четверг \n Пятница \n Суббота \n Воскресенье');
-    document.getElementById('tag-list').innerHTML = '<li>Понедельник<li>Вторник<li>Среда<li>Четверг<li>Пятница<li>Суббота<li>Воскресенье';
-    break;
-  case 'en':
-    console.log('Monday \n Tuesday \n Wednesday \n Thursday \n Friday \n Saturday \n Sunday');
-    document.getElementById('tag-list').innerHTML = '<li>Monday<li>Tuesday<li>Wednesday<li>Thursday<li>Friday<li>Saturday<li>Sunday';
-    break;
-  default:
-    console.log('А ведь указали, что писать');
-}
+// switch (lang) {
+//   case 'ru':
+//     console.log('Понедельник \n Вторник \n Среда \n Четверг \n Пятница \n Суббота \n Воскресенье');
+//     document.getElementById('tag-list').innerHTML = '<li>Понедельник<li>Вторник<li>Среда<li>Четверг<li>Пятница<li>Суббота<li>Воскресенье';
+//     break;
+//   case 'en':
+//     console.log('Monday \n Tuesday \n Wednesday \n Thursday \n Friday \n Saturday \n Sunday');
+//     document.getElementById('tag-list').innerHTML = '<li>Monday<li>Tuesday<li>Wednesday<li>Thursday<li>Friday<li>Saturday<li>Sunday';
+//     break;
+//   default:
+//     console.log('А ведь указали, что писать');
+// }
