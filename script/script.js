dd95324d797ecae91830e0a7a7d7dd54f49d698e
LayMorja <itkv@sk.ru>
2021-04-16 17:05:44 +0300

    Начал выполнять задания

--- script/script.js
+++ script/script.js
@@ -1,18 +1,102 @@
 'use strict';
-// let lang = prompt('Введите "ru" или "en"');
 
-// let namePerson = prompt('Введите имя', 'Артём');
-// namePerson === 'Артём' ? console.log('Директор') : namePerson === 'Максим' ? console.log('Преподователь') : console.log('Студент');
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
 
-// switch (lang) {
-//   case 'ru':
-//     console.log('Понедельник \n Вторник \n Среда \n Четверг \n Пятница \n Суббота \n Воскресенье');
-//     document.getElementById('tag-list').innerHTML = '<li>Понедельник<li>Вторник<li>Среда<li>Четверг<li>Пятница<li>Суббота<li>Воскресенье';
-//     break;
-//   case 'en':
-//     console.log('Monday \n Tuesday \n Wednesday \n Thursday \n Friday \n Saturday \n Sunday');
-//     document.getElementById('tag-list').innerHTML = '<li>Monday<li>Tuesday<li>Wednesday<li>Thursday<li>Friday<li>Saturday<li>Sunday';
-//     break;
-//   default:
-//     console.log('А ведь указали, что писать');
+let money,
+
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую (через пробел)', 'Кино, обучение, транспорт'),
+
+  deposit = prompt('Есть ли у вас депозит в банке?', 'нет'),
+
+  // expenses1 = prompt('Введите обязательную статью расходов', 'Детский сад'),
+  // amount1 = +prompt('Во сколько это обойдется?', 2500),
+
+  // expenses2 = prompt('Введите обязательную статью расходов', 'Домашние животные'),
+  // amount2 = +prompt('Во сколько это обойдется?', 3000),
+
+  mission = +prompt('Какова цель Ваших сбережений?', 150000),
+
+  income = 'Фриланс';
+
+switch (deposit) {
+  case 'да':
+  case 'есть':
+    deposit = true;
+    break;
+  default:
+    deposit = false;
+};
+
+let start = function(){
+  do {
+    money = prompt('Каков Ваш месячный доход?', 50000);
+  } 
+  while(!isNumber(money))
+};
+
+start();
+
+let expenses = [];
+
+let getExpensesMonth = function(){
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    
+    expenses[i] = prompt('Введите обязательную статью расходов', 'Домашние животные');
+
+    sum += +prompt('Во сколько это обойдется?', 3000);
+
+    while (!isNumber(sum)){
+      sum += +prompt('Во сколько это обойдется?', 3000);
+    }
+  }
+  console.log(expenses);
+  return sum;
+};
+
+let expensesAmount = getExpensesMonth();
+
+
+let showTypeOf = function(data){
+  return typeof data;
+};
+console.log(showTypeOf(money));
+console.log(showTypeOf(income));
+console.log(showTypeOf(deposit));
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+// const getExpensesMonth = function(){
+//   return amount1 + amount2;
 // }
+console.log('Сумма обязательных расходов за месяц: ', expensesAmount);
+
+const getAccumulatedMonth = function(a, b){
+  return money - expensesAmount;
+}
+let accumulatedMonth = getAccumulatedMonth();
+// console.log('Ваши накопления за месяц: ', accumulatedMonth)
+
+const getTargetMonth = function(){
+  return Math.ceil(mission / accumulatedMonth);
+}
+console.log('Ваш срок достижения цели: ', getTargetMonth(), 'месяцев');
+
+let budgetDay = Math.floor(accumulatedMonth / 30);
+console.log('Ваш дневной бюджет: ', budgetDay);
+
+const getStatusIncome = function(){
+  if (budgetDay >= 1200) {
+    return ('У Вас высокий уровень дохода');
+  } else if (1200 > budgetDay > 600) {
+    return ('У Вас средний уровень дохода');
+  } else if (600 >= budgetDay > 0) {
+    return ('У Вас низкий уровень дохода');
+  } else {
+    return ('Что-то пошло не так :/');
+  }
+}
+console.log(getStatusIncome());
